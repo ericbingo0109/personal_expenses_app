172ae69400e2c9aa6f12878bbbbd4f37730405ec
Eric Wu <eric_wu@ihergo.com>
2020-08-16 22:55:06 +0800

    以Container的height去控制SingleChildScrollView控制滾動範圍

--- lib/main.dart
+++ lib/main.dart
@@ -31,36 +31,38 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: SingleChildScrollView(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Container(
+        height: 400,
+        child: SingleChildScrollView(
           child: Column(
             /*
-            MainAxisAlignment : 順著Widget發展方向 去記
-            如Column : 上下方向 ; Row : 左右方向
-            CrossAxisAlignment : 反之
-            如Column : 左右方向 ; Row : 上下方向
-            */
+                MainAxisAlignment : 順著Widget發展方向 去記
+                如Column : 上下方向 ; Row : 左右方向
+                CrossAxisAlignment : 反之
+                如Column : 左右方向 ; Row : 上下方向
+                */
             crossAxisAlignment: CrossAxisAlignment.center, // default is center
             // mainAxisAlignment: MainAxisAlignment.spaceAround, // default is start
             children: <Widget>[
               /* 方法一 Card 裡面的child改用Container去包 然後設定width大小
-              Card(
-                // child: Text('Chart !'),
-                // 若想調整Card size 則必須在 child設定
-                child: Container(
-                  // width: 200,
-                  width: double.infinity, // take as much width as it can
-                  child: Text('Chart !'),
-                ),
-                color: Colors.blue,
-                elevation: 5, // 按鈕陰影大小值
-              ),
-                */
+                  Card(
+                    // child: Text('Chart !'),
+                    // 若想調整Card size 則必須在 child設定
+                    child: Container(
+                      // width: 200,
+                      width: double.infinity, // take as much width as it can
+                      child: Text('Chart !'),
+                    ),
+                    color: Colors.blue,
+                    elevation: 5, // 按鈕陰影大小值
+                  ),
+                    */
               /*方法二 改用Container 去包整個 Card
-               滑鼠游標在 Text 按 Command + . 選擇 wrap with Container
-              */
+                   滑鼠游標在 Text 按 Command + . 選擇 wrap with Container
+                  */
               Container(
                 width: double.infinity,
                 child: Card(
@@ -73,13 +75,15 @@ class MyHomePage extends StatelessWidget {
               // NewTransaction(),
               // TransactionList()
               /*
-              Card(
-                child: Text('List of tx '),
-                color: Colors.red,
-              )
-              */
+                  Card(
+                    child: Text('List of tx '),
+                    color: Colors.red,
+                  )
+                  */
             ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
