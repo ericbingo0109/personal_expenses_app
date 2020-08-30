faa36c077adc36ad0149854febebf250739d6b62
Eric Wu <eric_wu@ihergo.com>
2020-08-30 16:41:55 +0800

    用DateFormat.E(weekDay)顯示weekDay的shortcut

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
 import '../models/transaction.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
-  
+  final List<Transaction> currentTransactions;
+
+  Chart({this.currentTransactions});
+/**
+ * List<Map<String, Object>> 最後再打 不然vs code有點怪怪的一直報錯...
+ */
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      // Duration 可選days, hours, minutes, seconds,..etc
+      // substract 多久以前
+      // final weekDay = DateTime.now().add(Duration(days: index));
+      // add 多久之後
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      // import intl.dart for using DateFormat
+      return {'Day': DateFormat.E(weekDay), 'amount': 9.99};
+      // DateFormat.E(weekDay) 會顯示那天的shortcut ex: Monday -> M ; Tuesday -> T
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
