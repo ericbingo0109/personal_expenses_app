af79ae8e3071ff573d69604e266b4c300213a29d
Eric Wu <eric_wu@ihergo.com>
2020-08-30 17:50:34 +0800

    使用FractionallySizedBox做出動態比例的bar for our charts

---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double spendingAmount;
+  final double spendingPercentageOfTotal;
+
+  ChartBar(this.label, this.spendingAmount, this.spendingPercentageOfTotal);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        // 這邊設定chart僅顯示到整數部分
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        SizedBox(
+          height: 4,
+        ),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1),
+                  color: Color.fromRGBO(220, 220, 220, 1), // light grey 淺灰色
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              /**
+               * heightFactor 的值在 0 ~ 1 之間
+               * 1 means 100% of the surrounding Container which is h:60 , w:10
+               */
+              FractionallySizedBox(
+                heightFactor: spendingPercentageOfTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
+        Text(label),
+      ],
+    );
+  }
+}
